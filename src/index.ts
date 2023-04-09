41f3cb9584eed79528efbfe180684ab85f75b2db
JaimeGensler <jaimegensler0@gmail.com>
2023-04-09 10:16:56 -0700

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,19 @@ import ts from 'typescript';
 import { thyseusTransformer, type SystemParameterMap } from './transformer';
 import type { Plugin } from 'vite';
 
-interface PluginConfig {
-	systemParameters: SystemParameterMap;
+interface ThyseusPluginConfig {
+	systemParameters?: SystemParameterMap;
 }
 
-export function thyseusPlugin(config?: PluginConfig): Plugin {
-	let program: ts.Program;
-	let checker: ts.TypeChecker;
-	let transformer: ts.TransformerFactory<ts.SourceFile>;
+export function thyseusPlugin(config?: ThyseusPluginConfig): Plugin {
 	const printer = ts.createPrinter();
 
 	return {
 		name: '@thyseus/plugin-vite',
 		version: '0.12.0',
-		configResolved({ root }) {
-			program = ts.createProgram([root], { noEmit: true });
-			checker = program.getTypeChecker();
-			transformer = thyseusTransformer(program);
-		},
 		transform(_, id) {
+			const program = ts.createProgram([id], {});
+			const transformer = thyseusTransformer(program);
 			const sourceFile = program.getSourceFile(id)!;
 			const { transformed } = ts.transform(sourceFile, [transformer]);
 			return {
