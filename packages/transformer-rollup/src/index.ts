ef2bb659d4f719fd994bc2eb384dd866ac500971
JaimeGensler <jaimegensler0@gmail.com>
2023-06-04 13:08:44 -0700

    Fix types, v0.12.0

--- packages/transformer-rollup/src/index.ts
+++ packages/transformer-rollup/src/index.ts
@@ -1,28 +1,25 @@
 import { createFilter } from '@rollup/pluginutils';
 import {
 	getTransformer,
-	// type transformerConfig,
+	type TransformerConfig,
 } from '@thyseus/typescript-transformer';
 import type { Plugin } from 'vite';
 
-// TODO: Fix types, they got a little goofed beta.7
-
-type TransformerConfig = Parameters<typeof getTransformer>[0];
 type ThyseusPluginConfig = {
 	include?: string;
 	exclude?: string;
-} & Partial<TransformerConfig>;
+} & TransformerConfig;
 export function thyseusPlugin({
 	include = '**/*.ts',
 	exclude,
 	...transformerConfig
 }: ThyseusPluginConfig = {}) {
-	const transform = getTransformer(transformerConfig as any);
+	const transform = getTransformer(transformerConfig);
 	const filter = createFilter(include, exclude);
 
 	return {
 		name: '@thyseus/transformer-rollup',
-		version: '0.12.0-beta.7',
+		version: '0.12.0',
 		enforce: 'pre',
 		transform(file, id) {
 			if (!filter(id)) {
